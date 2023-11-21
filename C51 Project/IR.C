c0212b75fdb96686e28653cdf6deca75bf8d7ceb
Megre <renhao.x@seu.edu.cn>
2023-11-21 15:17:19 +0800

    double MOS
    DC12V/6A max

--- C51 Project/IR.C
+++ C51 Project/IR.C
@@ -1,11 +1,3 @@
-/********************************************************************************
-*  描述:                                                                        *
-*         数码管显示 遥控键值读取器                                             *
-*          遥控器接p3.2                                                         *
-*  现象：当按遥控器时，按下事 数码管会有闪烁，为什么会闪烁呢                    *
-*                                                                               *
-*         连接方法：使用红外功能时 J1跳线短接                                   *
-*****************************************************************************8**/
 
 #include <reg51.h>
 
@@ -16,7 +8,7 @@ void IRdelay(char x);  //x*0.14MS
 void beep();
 
 unsigned char  IRCOM[7];
-extern unsigned char Y0;
+extern unsigned char control_type;
 extern void display(unsigned char Key_Val);
 /*******************************************************************/
 void IRInit()
@@ -30,59 +22,64 @@ void IRInit()
 /**********************************************************/
 void IR_IN(void) interrupt 0 
 {
-  unsigned char j,k,N=0;
-     EX0 = 0;  
-	   
-	 IRdelay(5);
-	 if (IRIN==1) 
-     { EX0 =1;
-	   return;
-	  } 
-                           //确认IR信号出现
-  while (!IRIN)            //等IR变为高电平，跳过9ms的前导低电平信号。
-  {IRdelay(1);}
+    unsigned char j,k,N=0;
+    EX0 = 0;  
 
- for (j=0;j<4;j++)         //收集四组数据
- { 
-  for (k=0;k<8;k++)        //每组数据有8位
-  {
-   while (IRIN)            //等 IR 变为低电平，跳过4.5ms的前导高电平信号。
-     {IRdelay(1);}
-    while (!IRIN)          //等 IR 变为高电平
-     {IRdelay(1);}
-     while (IRIN)           //计算IR高电平时长
-      {
-    IRdelay(1);
-    N++;           
-    if (N>=30)
-	 { EX0=1;
-	 return;}                  //0.14ms计数过长自动离开。
-      }                        //高电平计数完毕                
-     IRCOM[j]=IRCOM[j] >> 1;                  //数据最高位补“0”
-     if (N>=8) {IRCOM[j] = IRCOM[j] | 0x80;}  //数据最高位补“1”
-     N=0;
-  }//end for k
+    IRdelay(5);
+    if (IRIN==1) 
+    { 
+        EX0 =1;
+        return;
+    } 
+                           //确认IR信号出现
+    while (!IRIN)            //等IR变为高电平，跳过9ms的前导低电平信号。
+    {IRdelay(1);}
+
+    for (j=0;j<4;j++)         //收集四组数据
+    { 
+        for (k=0;k<8;k++)        //每组数据有8位
+        {
+            while (IRIN)            //等 IR 变为低电平，跳过4.5ms的前导高电平信号。
+            {IRdelay(1);}
+            while (!IRIN)          //等 IR 变为高电平
+            {IRdelay(1);}
+            while (IRIN)           //计算IR高电平时长
+            {
+                IRdelay(1);
+                N++;           
+                if (N>=30)
+                { 
+                    EX0=1;
+                    return;
+                }                  //0.14ms计数过长自动离开。
+            }                        //高电平计数完毕                
+            IRCOM[j]=IRCOM[j] >> 1;                  //数据最高位补“0”
+            if (N>=8) {IRCOM[j] = IRCOM[j] | 0x80;}  //数据最高位补“1”
+            N=0;
+            
+            control_type = 'r';
+        }//end for k
    	
- }//end for j
+    }//end for j
    
-   if (IRCOM[2]!=~IRCOM[3])
-   { EX0=1;
-     return; 
-	 
+    if (IRCOM[2]!=~IRCOM[3])
+    { 
+        EX0=1;
+        return; 
 	}
 
-       EX0 = 1; 
+    EX0 = 1; 
 } 
 
 
 /**********************************************************/
 void IRdelay(unsigned char x)    //x*0.14MS
 {
- unsigned char i;
-  while(x--)
- {
-  for (i = 0; i<13; i++) {}
- }
+    unsigned char i;
+    while(x--)
+    {
+    for (i = 0; i<13; i++) {}
+    }
 }
 
 
--- C51 Project/main.C
+++ C51 Project/main.C
@@ -1,11 +1,3 @@
-/********************************************************************************
-*  描述:                                                                       *
-*         数码管显示 遥控键值读取器                                            *
-*         遥控器接p3.2                                                         *
-*                                                                              *
-*                                                                              *
-*                                                                              *
-*******************************************************************************/
 #include <intrins.h>
 #include <reg52.h>
 #define  NOP() _nop_()  /* 定义空指令 */
@@ -28,61 +20,61 @@ void initSerial();
 
 main()
 { 
-  IRInit();
-  initSerial();
+    IRInit();
+    initSerial();
 
-  ctrl0 = 0;
-  ctrl1 = 0;
-  ctrl2 = 0;
-  PWR_LED = 0; // Power LED on
+    ctrl0 = 0;
+    ctrl1 = 0;
+    ctrl2 = 0;
+    PWR_LED = 0; // Power LED on
     while(1)
     { 
-    // 接收红外并控制
-    if(control_type == 'r') {
-      shutdown(IRCOM[2]);       
-    }
-    
-    // 串口控制
-    else if (Flag==1)
-    {
-       EA=0; // 关总中断 
+      // 接收红外并控制
+      if(control_type == 'r') {
+        shutdown(IRCOM[2]);       
+      }
       
-       if((ReData & 0x80) == 0x80) { // 最高位为1，控制开关
-          ctrl0 = ((ReData & 1)==1?1:0);
-          ctrl1 = ((ReData & 0x02)==0x02?1:0);
-          ctrl2 = ((ReData & 0x04)==0x04?1:0);
-       }
+      // 串口控制
+      else if (Flag==1)
+      {
+         EA=0; // 关总中断 
+        
+         if((ReData & 0x80) == 0x80) { // 最高位为1，控制开关
+            ctrl0 = ((ReData & 1)==1?1:0);
+            ctrl1 = ((ReData & 0x02)==0x02?1:0);
+            ctrl2 = ((ReData & 0x04)==0x04?1:0);
+         }
 
-       SenData = (ReData & 0x80) | ctrl0 | (ctrl1*2) | (ctrl2*4);
-       
-       // 发送数据
-       SBUF = SenData;  // SUBF接受/发送缓冲器
-       while(TI==0);
-       TI=0; // 发送中断标志
-       Flag=0;
-       EA=1; // 开总中断
-    }
-  } 
+         SenData = (ReData & 0x80) | ctrl0 | (ctrl1*2) | (ctrl2*4);
+         
+         // 发送数据
+         SBUF = SenData;  // SUBF接受/发送缓冲器
+         while(TI==0);
+         TI=0; // 发送中断标志
+         Flag=0;
+         EA=1; // 开总中断
+      }
+    } 
 }
 
 
 void shutdown(unsigned char key)
 { 
-  if(key == 69)  { // 全开 
+    if(key == 69)  { // 全开 
         if(ctrl0 != 1) ctrl0 = 1;
         if(ctrl1 != 1) ctrl1 = 1;
-    if(ctrl2 != 1) ctrl2 = 1;
+        if(ctrl2 != 1) ctrl2 = 1;
     }
     else if(key == 71) { // 全关
         if(ctrl0 != 0) ctrl0 = 0;
         if(ctrl1 != 0) ctrl1 = 0;
-    if(ctrl2 != 0) ctrl2 = 0;
+        if(ctrl2 != 0) ctrl2 = 0;
     }
     else if(key == 12){
         if(ctrl0 != 1) ctrl0 = 1;
     }
     else if(key == 24) {
-    if(ctrl1 != 1) ctrl1 = 1;
+        if(ctrl1 != 1) ctrl1 = 1;
     }
 }
 
@@ -94,18 +86,18 @@ void delay(unsigned int i)
 }
 
 void initSerial() {
-    SCON = 0x50;       // 串口方式1, 10位异步收发。REN=1允许串行接受状态
-      TMOD|= 0x20;       // 定时器工作方式2，8位常数自动装入
-    PCON|= 0x80;       // 波特率倍增
-        //TH1 = 0xFD;     // baud*2  波特率19200、数据位8、停止位1。效验位无 (11.0592)
+    SCON = 0x50;      // 串口方式1, 10位异步收发。REN=1允许串行接受状态
+    TMOD|= 0x20;      // 定时器工作方式2，8位常数自动装入
+    PCON|= 0x80;      // 波特率倍增
+    //TH1 = 0xFD;     // baud*2  波特率19200、数据位8、停止位1。效验位无 (11.0592)
     //TL1 = 0xFD;
-        //TH1 = 0xF3;                // baud*2  波特率4800、数据位8、停止位1。效验位无 (12M)
+    //TH1 = 0xF3;     // baud*2  波特率4800、数据位8、停止位1。效验位无 (12M)
     //TL1 = 0xF3;
-    TH1 = 0xF4;     // baud*2  波特率4800、数据位8、停止位1。效验位无 (11.0592)
+    TH1 = 0xF4;       // baud*2  波特率4800、数据位8、停止位1。效验位无 (11.0592)
     TL1 = 0xF4;
-        TR1  = 1;        // TCON (D6)：定时器T1启动
-        ES   = 1;        // 开串口中断
-        EA   = 1;        // 开总中断
+    TR1  = 1;         // TCON (D6)：定时器T1启动
+    ES   = 1;         // 开串口中断
+    EA   = 1;         // 开总中断
 }
                                                     
 /****************************************************
@@ -115,7 +107,7 @@ void ser_int (void) interrupt 4 using 1
 {
     if(RI == 1)        // 接收中断标志
     {
-    control_type = 'u';
+        control_type = 'u';
     
         RI = 0;         // 清除RI接受中断标志
         ReData = SBUF;  // SUBF接受/发送缓冲器
